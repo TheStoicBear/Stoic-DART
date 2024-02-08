6a7d766a72cb23a64d439a6a7a5b19c503de4c4b
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-02-08 13:53:50 -0800

    Update server.lua

--- source/server.lua
+++ source/server.lua
@@ -1,3 +1,5 @@
+QBCore = exports['qb-core']:GetCoreObject()
+
 QBCore.Functions.CreateUseableItem('dart_item', function(source, item)
     local Player = QBCore.Functions.GetPlayer(source)
 	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
